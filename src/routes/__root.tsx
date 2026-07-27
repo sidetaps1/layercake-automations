dc6117ee6c620cb4b929e00735a7947d04688350
sidetaps1 <sidetaps1@gmail.com>
2026-07-26 21:47:25 -0300

    Google analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -118,6 +118,19 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FRK1PNT4E3" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-FRK1PNT4E3');
+            `,
+          }}
+        />
         <HeadContent />
       </head>
       <body>
